817a29d383e8a3c0782af1ed7194f4f7dd8cdd5a
zxc8612253 <71476349+zxc8612253@users.noreply.github.com>
2021-05-25 23:58:43 +0800

    素材COPY一半

---
+++ 2D_Final_0608481/Assets/Scripts/Enemy1.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("追蹤範圍"), Range(0, 500)]
+    public float rangeTrack = 2;
+    [Header("攻擊範圍"), Range(0, 50)]
+    public float rangeAttack = 0.5f;
+    [Header("移動速度"), Range(0, 50)]
+    public float speed = 2;
+    [Header("攻擊特效")]
+    public ParticleSystem psAttack;
+    [Header("攻擊冷卻時間"), Range(0, 10)]
+    public float cdAttack = 3;
+    [Header("攻擊力"), Range(0, 1000)]
+    public float attack = 20;
+    [Header("經驗值"), Range(0, 500)]
+    public float exp = 30;
+
+    private Transform player;
+    private Player _player;
+
+    /// <summary>
+    /// 計時器
+    /// </summary>
+    private float timer;
+
+    [Header("血量")]
+    public float hp = 200;
+    [Header("血條系統")]
+    public HpManager hpManager;
+    [Header("角色是否死亡")]
+    public bool isDead = false;
+
+    private float hpMax;
+
+    private void Start()
+    {
+        hpMax = hp;   // 取得血量最大值
+        player = GameObject.Find("玩家").transform;
+        _player = player.GetComponent<Player>();
+    }
+
+    // 繪製圖示事件 : 在Unity內顯示輔助開發
+    private void OnDrawGizmos()
+    {
+
+        // 先指定顏色在畫圖
+        Gizmos.color = new Color(0, 0, 1, 0.3f);
+        // 繪製球體(中心點, 半徑)
+        Gizmos.DrawSphere(transform.position, rangeTrack);
+
+        Gizmos.color = new Color(1, 0, 0, 0.3f);
+        Gizmos.DrawSphere(transform.position, rangeAttack);
+    }
+
+    private void Update()
+    {
+        Track();
+    }
+
+    /// <summary>
+    /// 追蹤玩家
+    /// </summary>
+    private void Track()
+    {
+        if (isDead) return;
+        
+        // 距離 等於 三維向量的距離(A點 , B點)
+        float dis = Vector3.Distance(transform.position, player.position);
+
+        // 如果 距離 小於等於 攻擊範圍 進入攻擊狀態
+        // 如果 距離 小於等於 追蹤範圍 才開始追蹤
+        if(dis <= rangeAttack)
+        {
+            Attack();
+        }
+
+        else if (dis <= rangeTrack)
+        {
+            // 物件的座標 更新為 三維向量 的 往前移動(物件 的 座標 , 目標 的 座標 , 速度 * 一幀的時間)
+            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+    }
+
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    private void Attack()
+    {
+        timer += Time.deltaTime;  // 累加時間
+
+        // 如果 計時器 大於等於 冷卻時間 就攻擊
+        if(timer >= cdAttack)
+        {
+            timer = 0;             // 計時器歸零
+            psAttack.Play();       // 播放 攻擊特效
+            Collider2D hit = Physics2D.OverlapCircle(transform.position, rangeAttack, 1<< 9);
+            hit.GetComponent<Player>().Hit(attack);
+        } 
+    }
+    
+    
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    /// <param name="damage">接收到的傷害值</param>
+    public void Hit(float damage)
+    {
+        hp -= damage;                            // 扣除傷害值               
+        hpManager.UpdateHpBar(hp, hpMax);        // 更新血條
+        StartCoroutine(hpManager.ShowDamage(damage));  // 啟動協同程序 (顯示傷害值())
+
+        if (hp <= 0) Dead();                           // 如果 血量 <= 0 就死亡
+    }
+
+
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    private void Dead()
+    {
+        if (isDead) return;                            // 如果死亡就退出
+        hp = 0;
+        isDead = true;
+        Destroy(gameObject, 1.5f);
+        _player.Exp(exp);                              // 將經驗值傳給玩家
+    }
+}
---
+++ 2D_Final_0608481/Assets/Scripts/HpManager1.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections; // 引用 系統集合 API 裡面包含協同程序
+
+public class HpManager : MonoBehaviour
+{
+    [Header("血條")]
+    public Image bar;
+    [Header("傷害數值")]
+    public RectTransform rectDamage;
+
+
+    /// <summary>
+    /// 更新血量與血量最大值並更新血條
+    /// </summary>
+    /// <param name="hp">當前血量</param>
+    /// <param name="hpMax">血量最大值</param>
+    public void UpdateHpBar(float hp, float hpMax)
+    {
+        // 血條.填滿數值 = 當前血量 / 血量最大值
+        bar.fillAmount = hp / hpMax;
+    }
+
+    public IEnumerator ShowDamage(float damage)
+    {
+        RectTransform rect = Instantiate(rectDamage, transform); // 生成傷害數值在血條系統內
+        rect.anchoredPosition = new Vector2(0, 200);             // 指定座標
+        rect.GetComponent<Text>().text = damage.ToString();        // 更新數值
+
+        float y = rect.anchoredPosition.y;                       // 取得原始 Y 軸
+
+        while(y < 400)                                           // 當 Y 小於 400 時持續執行
+        {
+            y += 20;                                             // 每次上升的單位 20
+            rect.anchoredPosition = new Vector2(0, y);           // 更新座標
+            yield return new WaitForSeconds(0.02f);              //等待幾秒(秒數)
+        }
+        Destroy(rect.gameObject, 0.5f);
+    }
+}
